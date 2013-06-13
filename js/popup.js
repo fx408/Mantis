38afde489d13e2138a2061a88c6b59597b90fa87
老陌 <664712890@qq.com>
2013-06-13 17:57:36 +0800

    修复BUG索引 链接错误

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,26 @@
 function showList() {
 	
 	// 显示列表
-	this.show = function(data) {
+	this.show = function(data, preUrl) {
 		html = "";
 		
 		for(var i =0; i < data.length; i++) {
 			if(!data[i].title.text) continue;
 			
 			html += '<h4><a href="javascript:;" class="bugTitle">'+data[i].title.text+' (<span>'+data[i].list.length+'</span>)</a></h4>';
-			html += this.createHtml(data[i].list);
+			html += this.createHtml(data[i].list, preUrl);
 		}
 		
 		$("#topicList").html(html);
 	}
 	
 	// 生成HTML
-	this.createHtml = function(data) {
-		
+	this.createHtml = function(data, preUrl) {
 		var html = '<div class="tab-pane active" style="display:none">'
 			+ '<table class="table table-hover">';
 		for(var k in data) {
+			if(!/^\w+\:\/\//.test(data[k].url)) data[k].url = preUrl+"/"+data[k].url;
+			
 			html += '<tr><td>'
 				+ '<a href="'+data[k].url+'" target="_blank">'+data[k].content+'</a>'
 				+ '</td></tr>';
@@ -40,7 +41,8 @@ $(function() {
 		APP.request(url, {
 			callback: function(err, data) {
 				if(false != err) return false;
-				SL.show(data);
+				url = url.match(/(.*?)\/\/([^\/]+)\//);
+				SL.show(data, url[1]+"//"+url[2]);
 			},
 			isString: true
 		});
